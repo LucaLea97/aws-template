b1afd4dc1bf5e512684a36541d62670467224986
LucaLea97 <luca.domenichelli@lea-renergy.com>
2025-03-12 13:08:21 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,6 @@ const client = generateClient<Schema>();
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
     <main>
       <h1>My todos</h1>
