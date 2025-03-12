40628b708efaaf994246229db2863ec26b7b9af0
LucaLea97 <luca.domenichelli@lea-renergy.com>
2025-03-12 14:36:07 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,54 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+feat/mqtt
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
+import supabase from "./utils/supabase.js";
+import { useMqtt } from "./hooks/useMqtt.js";
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+    const { ledState, sendMessage } = useMqtt();
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-    </main>
-  );
+    useEffect(() => {
+        async function checkUser() {
+            const { data, error } = await supabase.auth.getUser();
+            if (error || !data.user) {
+                navigate('/login'); // Redirect to login page
+            } else {
+                setUser(data.user);
+            }
+        }
+        checkUser();
+    }, [navigate]);
+
+    if (!user) return null; // Prevent UI flicker before redirect
+
+    async function handleLogout() {
+        await supabase.auth.signOut();
+        navigate('/login');
+    }
+
+    return (
+        <>
+            <main>
+                <h1>Clelia</h1>
+                <h3>Hello {user.email}</h3>
+                <button onClick={() => sendMessage(1, "ON")}>Turn on Led</button>
+                <button onClick={() => sendMessage(1, "OFF")}>Turn off Led</button>
+                <aside>
+                    <button> Map </button>
+                    <button> Controls </button>
+                </aside>
+                <aside style={{marginTop: '2rem'}}>
+                    <p className="text-lg">LED ID: {ledState.id || "None"}</p>
+                    <p className="text-lg">Current State: {ledState.state}</p>
+                </aside>
+                <button onClick={handleLogout}>Logout</button>
+                <footer>Lea Renergy s.r.l.</footer>
+            </main>
+        </>
+    );
 }
 
 export default App;
